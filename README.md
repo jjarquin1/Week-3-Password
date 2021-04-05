d8a68fe37becce2c8fdd4c9b5aa3145db85425af
jkooda <77827825+jjarquin1@users.noreply.github.com>
2021-04-04 20:05:39 -0700

    used Charcode to generate random characters. created a generate function to generate password using if statements and for loops. finished the README.md file.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Week-3-Password
 Creating a random password generator 
+
+#javascript 
+
+Used CharCode chart to randomize characters; letters, numbers and  symbols, then assigned each of them to a variable. 
+
+
+Under generatePassword() 
+
+Used window prompt for user input of how many characters to include and filtered out invalid values with and "if" "else" statement. 
+
+Made confirm windows to verify if, uppercase, lowercase, symbols and numbers would be included in the password. 
+
+created a var with an empty string for password input
+Created a new function generate() to propogate password
+
+
+Under generate()
+
+created a variable that would string together all options from the the window prompts.
+
+used "if" statements to verify user input and then assign it to passWrd variable with addition assignment operator. 
+
+used a for loop to mulitply and randomize characters until it meets password length criteris.
+
+executed the funtion to generate password
+
+return, returns passWrd value 
--- script.js
+++ script.js
@@ -1,31 +1,75 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var pswdLength = prompt("How many characters in your password?")
-var upperCase = confirm("Ok confirms uppercase characters")
-var lowerCase = confirm("Ok confirms lowercase characters")
-var spclChar= confirm("Ok confirms special chacratcers")
-var numeric = confirm("Ok confirms numeric chacracters")
 
 // Write password to the #password input
+
+function generatePassword() {
+ 
+  //Variables generate random characters using CharCodes 
+  var UpperCase_Char = String.fromCharCode(Math.floor(Math.random()*26)+65)
+  var LowerCase_Char = String.fromCharCode(Math.floor(Math.random()*26)+97)
+  var Number_Char = String.fromCharCode(Math.floor(Math.random()*10)+48)
+  var Symbol_Char = String.fromCharCode(Math.floor(Math.random()*14)+33)
+ 
+  //Prompt windows and confirm windows define the criteria 
+  var pswdLength = Number(prompt("How many characters in your password?"));
+  //Checks to see if user input meets criteria before moving on
+  if (pswdLength<8||pswdLength>128){
+    return("Not a Valid Input");
+  } else {
+    console.log(pswdLength);
+  }
+   var upperCase = confirm("Ok confirms uppercase characters");
+
+   var lowerCase = confirm("Ok confirms lowercase characters");
+  
+   var spclChar= confirm("Ok confirms special chacratcers");
+  
+   var numeric = confirm("Ok connfirms numbers");
+   
+   var passWrd="";
+   
+   function generate (){
+  
+   var Type= UpperCase_Char+LowerCase_Char+Number_Char+Symbol_Char;
+
+   if (upperCase===true) {
+    passWrd += UpperCase_Char;
+    console.log (UpperCase_Char);
+  }
+
+  if (lowerCase===true) {
+    passWrd += LowerCase_Char;
+    console.log (LowerCase_Char);
+  }
+
+  if (spclChar===true){
+    passWrd += Symbol_Char;
+    console.log(Symbol_Char);
+  }
+
+  if (numeric===true) {
+    passWrd += Number_Char;
+    console.log (Number_Char);
+    }
+   
+   for(i=passWrd.length; i < pswdLength; i++) {
+       passWrd += Type;
+   }
+  
+  }
+ 
+generate();
+
+return passWrd; 
+
+};
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
- if (upperCase===true) {
-    return String.fromCharCode(Math.floor(Math.random()*26)+65);
- }
- if (lowerCase===true) {
-    return String.fromCharCode(Math.floor(Math.random()*26)+97);
- }
- if (spclChar===true) {
-    const symbols = '!@#$%^&*(){}[]=<>/';
-    return symbols[Math.floor(Math.random()*symbols.length)];
- }
- if (numeric===true) {
-  return  Math.floor(Math.random() * 10);
- }
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
